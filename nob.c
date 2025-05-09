29a5537d71f4fe0701c05407d904fc74b7c4c560
amtoine <stevan.antoine@gmail.com>
2025-05-09 12:27:15 +0200

    complete nob.c

--- nob.c
+++ nob.c
@@ -1,16 +1,36 @@
 #define NOB_IMPLEMENTATION
 #include "nob.h"
 
+#include <openssl/sha.h>
+#include <stdio.h>
+
 int main(int argc, char **argv) {
   NOB_GO_REBUILD_URSELF(argc, argv);
 
   Nob_Cmd cmd = {0};
+  Nob_String_Builder sb = {0};
 
   nob_mkdir_if_not_exists("build/");
 
+  // compile
   nob_cmd_append(&cmd, "gcc", "-O3", "-o", "build/main", "main.c");
   if (!nob_cmd_run_sync_and_reset(&cmd))
     return 1;
+
+  // hash binary
+  nob_read_entire_file("build/main", &sb);
+  unsigned char *md_buf = malloc(SHA256_DIGEST_LENGTH);
+  unsigned char *digest =
+      SHA256((const unsigned char *)sb.items, sb.count, md_buf);
+  sb.count = 0;
+  for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+    nob_sb_appendf(&sb, "%02x", digest[i]);
+  }
+
+  // copy
+  char *dst_path = malloc(5 + 1 + 15 + SHA256_DIGEST_LENGTH * 2);
+  sprintf(dst_path, "build/find-git-repos-%s", sb.items);
+  nob_copy_file("build/main", dst_path);
 
   return 0;
 }
