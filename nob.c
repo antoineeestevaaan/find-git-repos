bbb78ccdbf4cb8bfbf3140dd501d650c4ae4f4da
antoineeestevaaan <antoineee.stevaaan@gmail.com>
2025-07-12 09:22:46 +0200

    add build pipeline with nob.h and hash source
    adds nob.h and its nob.c to compile the code
    - nob.h has been modified to provide extra -l dependencies to cc
    - nob.c will put the compilation artifacts of compiling main.c inside build/
    - nob.c will hash the source code (main.c) being compiled to add that to the output binary
    - a simple README explaining the pipeline has been added
    
    ## Commits
    - **add nob.h**
    - **add nob.c**
    - **use nob.h directly**
    - **compile with SSL and CRYPTO**
    - **complete nob.c**
    - **nob: define BUILD_DIR and MAIN**
    - **refactor "executable" things in constants**
    - **add a README**
    - **complete NOB bootstrap snippet**
    - **nob.h: do not format**
    - **nob.h: restore (-lssl, -lcrypto)**
    - **nob.h: add support for extra compilation options**
    - **add -lssl and -lcrypto to nob.c**
    - **hash source code instead of binary when building**

---
+++ nob.c
@@ -0,0 +1,47 @@
+#define NOB_IMPLEMENTATION
+#include "nob.h"
+
+#include <openssl/sha.h>
+#include <stdio.h>
+
+#define BUILD_DIR "build/"
+#define MAIN (BUILD_DIR "main")
+#define EXE_TEMPLATE                                                           \
+  (BUILD_DIR "find-git-repos-%s") // "%s" will be replaced with the hash of the
+                                  // executable
+#define EXE_LENGTH                                                             \
+  5 +                          /* build directory                      */      \
+      1 +                      /* path separator                       */      \
+      15 +                     /* executable name                      */      \
+      SHA256_DIGEST_LENGTH * 2 /* hash length, two characters per byte */
+
+int main(int argc, char **argv) {
+  NOB_GO_REBUILD_URSELF_PLUS(argc, argv, NULL, "-lssl", "-lcrypto");
+
+  Nob_Cmd cmd = {0};
+  Nob_String_Builder sb = {0};
+
+  nob_mkdir_if_not_exists(BUILD_DIR);
+
+  // compile
+  nob_cmd_append(&cmd, "gcc", "-O3", "-o", MAIN, "main.c");
+  if (!nob_cmd_run_sync_and_reset(&cmd))
+    return 1;
+
+  // hash source code
+  nob_read_entire_file("main.c", &sb);
+  unsigned char *md_buf = malloc(SHA256_DIGEST_LENGTH);
+  unsigned char *digest =
+      SHA256((const unsigned char *)sb.items, sb.count, md_buf);
+  sb.count = 0;
+  for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+    nob_sb_appendf(&sb, "%02x", digest[i]);
+  }
+
+  // copy
+  char *exe = malloc(EXE_LENGTH);
+  sprintf(exe, EXE_TEMPLATE, sb.items);
+  nob_copy_file(MAIN, exe);
+
+  return 0;
+}
